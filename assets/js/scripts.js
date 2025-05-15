172b74468d307ef5f4cb1424fd93dfd428a48b62
Muhammad Zaky <zakimuhammad184@gmail.com>
2025-05-15 16:42:02 +0700

    push semua file

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,11 @@
-function myFunction() {
-    alert("kan sudah blg jangan diklik");
+<script>
+function updateClock() {
+  const now = new Date();
+  const timeString = now.toLocaleTimeString('en-GB', { hour12: false });
+  document.getElementById('clock').textContent = timeString;
 }
+setInterval(updateClock, 1000);
+updateClock(); // initial call
+</script>
+
+
